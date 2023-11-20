3e62a681ec66a2d79df55dcd8fcfd8c83c2b0a56
Vadim Goloubev <goloubev@hotmail.com>
2023-11-20 17:44:12 -0500

    Commit

---
+++ database/seeders/SuperUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class CreateSuperUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        /** @var User $user */
+        $user = User::create([
+            'name'  => 'Vadim Goloubev',
+            'email' => 'goloubev@hotmail.com',
+            'password' => Hash::make('123'),
+            'email_verified_at' => now(),
+        ]);
+
+        $user->assignRole('super-user');
+    }
+}
