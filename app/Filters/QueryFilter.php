a18d405415d2c4697b9df6d5302612c07ce9f702
Vadim Goloubev <goloubev@hotmail.com>
2023-11-23 17:12:10 -0500

    Commit

--- app/Filters/QueryFilter.php
+++ app/Filters/QueryFilter.php
@@ -9,33 +9,23 @@ class QueryFilter
 {
     public Request $request;
     protected Builder $builder;
-    protected string $delimiter = ',';
 
     public function __construct(Request $request)
     {
         $this->request = $request;
     }
 
-    public function filters(): array|string|null
-    {
-        return $this->request->query();
-    }
-
     public function apply(Builder $builder): Builder
     {
         $this->builder = $builder;
+        $filters = $this->request->query();
 
-        foreach ($this->filters() as $name => $value) {
-            if (method_exists($this, $name)) {
-                call_user_func_array([$this, $name], array_filter([$value]));
+        foreach ($filters as $key => $value) {
+            if (method_exists($this, $key)) {
+                call_user_func_array([$this, $key], array_filter([$value]));
             }
         }
 
         return $this->builder;
-    }
-
-    protected function paramToArray($param): array
-    {
-        return explode($this->delimiter, $param);
     }
 }
